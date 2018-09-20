62bdda3f753b08c843d937ca5b3bebbab366bb88
Alex Burr <aburr@pharos.com>
2018-09-20 10:13:12 -0400

    Move SVG logic into its own class

---
+++ src/ts/svgGenerator.ts
@@ -0,0 +1,97 @@
+namespace swingChords {
+    
+    export class svgGenerator {        
+        svgns: string = "http://www.w3.org/2000/svg";
+
+        // Construction ------------------------------------------------
+        constructor() {
+        }
+
+        // Public methods ----------------------------------------------
+        public gridBox(): Element {
+            return this.drawGridBox();
+        }
+
+        public note(noteObject: Note): Element {
+            return this.drawNote(noteObject);
+        }
+
+        // Private methods ---------------------------------------------
+        private drawNote(noteObject: Note): Element {
+            var note: Element;
+            var dotBox: Element;
+            var dotText: Element;
+            
+            dotBox = document.createElementNS(this.svgns, "svg");
+            dotBox.setAttributeNS(null, "class", "dotBox");
+            dotBox.classList.add("string" + noteObject.string);
+            dotBox.classList.add("fret" + noteObject.fret);
+
+            note = document.createElementNS(this.svgns, "circle");
+            note.setAttributeNS(null, "class", "dot");
+            note.setAttributeNS(null, "cx", "50%");
+            note.setAttributeNS(null, "cy", "50%");
+            note.setAttributeNS(null, "r", "50%");
+
+            dotText = document.createElementNS(this.svgns, "text");
+            dotText.setAttributeNS(null, "x", "50%");
+            dotText.setAttributeNS(null, "y", "52%");
+            dotText.setAttributeNS(null, "text-anchor", "middle");
+            dotText.setAttributeNS(null, "dy", ".3em");
+            dotText.innerHTML = noteObject.interval;
+
+            dotBox.appendChild(note);
+            dotBox.appendChild(dotText);
+            
+            if (noteObject.interval == "R") {
+                dotBox.classList.add("root");
+            }
+
+            return dotBox;
+        }
+
+        private drawGridBox(): Element {
+            var gridBox: Element;
+            var grid: Element;
+
+            gridBox = document.createElementNS(this.svgns, "svg");     
+            gridBox.setAttributeNS(null, "class", "gridBox");            
+            gridBox.setAttributeNS(null, "width", "100%");            
+            gridBox.setAttributeNS(null, "height", "100%");
+            
+            grid = document.createElementNS(this.svgns, "grid");
+            grid.setAttributeNS(null, "x", "0");
+            grid.setAttributeNS(null, "y", "0");
+
+            gridBox.appendChild(grid);
+
+            for (var x = 0; x < 100; x = x + 20) {
+                this.drawGridLines(gridBox, x);
+            }
+            
+            return gridBox;
+        }
+
+        private drawGridLine(classValue: string, xValue: string, yValue: string): Element {
+            var gridLine: Element; 
+            
+            gridLine = document.createElementNS(this.svgns, "rect");
+            gridLine.setAttributeNS(null, "class", classValue);
+            gridLine.setAttributeNS(null, "x", xValue);
+            gridLine.setAttributeNS(null, "y", yValue);
+            return gridLine;
+        }
+
+        private drawGridLines(gridBox: Element, counter: number): void {            
+            var xValue: string = (counter > 0) ? counter.toString() + "%" : counter.toString();
+
+            gridBox.appendChild(this.drawGridLine("grid", "0", "0"));
+
+            for (var y = 0; y < 100; y = y + 20) {
+                var yValue: string = (y > 0) ? y.toString() + "%" : y.toString();
+                var gridLine: Element = this.drawGridLine("gridLine", xValue, yValue);
+                gridBox.appendChild(gridLine);
+            }
+        }
+    }
+}
--- src/ts/swingChords.ts
+++ src/ts/swingChords.ts
@@ -3,12 +3,13 @@ namespace swingChords {
     export class app {
         body: HTMLBodyElement;
         itemTemplate: any;
-        svgns: string = "http://www.w3.org/2000/svg";
+        svgGenerator: svgGenerator;
 
         // Construction ------------------------------------------------
         constructor() {
             this.body = document.querySelector("body");
             this.itemTemplate = document.querySelector("#itemTemplate");
+            this.svgGenerator = new svgGenerator();
 
             this.drawChordGroups(allChords);
         }
@@ -24,97 +25,25 @@ namespace swingChords {
         private drawHeader(text: string): void {
             var header: HTMLHeadingElement = document.createElement("h2");
             var headerText: Text = document.createTextNode(text); 
+            
             header.appendChild(headerText);
             this.body.appendChild(header);
         }
 
-        private gridLine(classValue: string, xValue: string, yValue: string): Element {
-
-            var gridLine: Element = document.createElementNS(this.svgns, "rect");
-            gridLine.setAttributeNS(null, "class", classValue);
-            gridLine.setAttributeNS(null, "x", xValue);
-            gridLine.setAttributeNS(null, "y", yValue);
-            return gridLine;
-        }
-
-        private drawGridLines(gridBox: Element, counter: number): void {
-            
-            var xValue: string = (counter > 0) ? counter.toString() + "%" : counter.toString();
-
-            gridBox.appendChild(this.gridLine("grid", "0", "0"));
-            gridBox.appendChild(this.gridLine("gridLine", xValue, "0"));
-            gridBox.appendChild(this.gridLine("gridLine", xValue, "20%"));
-            gridBox.appendChild(this.gridLine("gridLine", xValue, "40%"));
-            gridBox.appendChild(this.gridLine("gridLine", xValue, "60%"));
-            gridBox.appendChild(this.gridLine("gridLine", xValue, "80%"));
-        }
-        
-        private gridBox(): Element {
-            var gridBox = document.createElementNS(this.svgns, "svg");     
-            gridBox.setAttributeNS(null, "class", "gridBox");            
-            gridBox.setAttributeNS(null, "width", "100%");            
-            gridBox.setAttributeNS(null, "height", "100%");
-
-            var grid: Element = document.createElementNS(this.svgns, "grid");
-            grid.setAttributeNS(null, "x", "0");
-            grid.setAttributeNS(null, "y", "0");
-
-            gridBox.appendChild(grid);
-
-            for (var x = 0; x < 100; x = x + 20) {
-                this.drawGridLines(gridBox, x);
-            }
-            
-            return gridBox;
-        }
-
-        private drawNote(box: any, noteObject: Note): void {
-            var note: Element;
-            var dotBox: any;
-            var dotText: any;
-            
-            dotBox = document.createElementNS(this.svgns, "svg");
-            dotBox.setAttributeNS(null, "class", "dotBox");
-
-            note = document.createElementNS(this.svgns, "circle");
-            note.setAttributeNS(null, "class", "dot");
-            note.setAttributeNS(null, "cx", "50%");
-            note.setAttributeNS(null, "cy", "50%");
-            note.setAttributeNS(null, "r", "50%");
-
-            dotText = document.createElementNS(this.svgns, "text");
-            dotText.setAttributeNS(null, "x", "50%");
-            dotText.setAttributeNS(null, "y", "52%");
-            dotText.setAttributeNS(null, "text-anchor", "middle");
-            dotText.setAttributeNS(null, "dy", ".3em");
-
-            dotBox.appendChild(note);
-            dotBox.appendChild(dotText);            
-
-            dotBox.classList.add("string" + noteObject.string);
-            dotBox.classList.add("fret" + noteObject.fret);
-            
-            if (noteObject.interval == "R") {
-                dotBox.classList.add("root");
-            }
-            dotText.innerHTML = noteObject.interval;
-            
-            box.appendChild(dotBox);
-        }
-
         private drawChord(chord: Chord, container: HTMLDivElement): void {
             var item: any;
-            var gridWrap: any;
+            var gridWrap: any;            
+            var gridBox = this.svgGenerator.gridBox();
             
             item = document.importNode(this.itemTemplate.content, true);
             item.querySelector("h3").innerHTML = chord.name;
 
             gridWrap = item.querySelector(".gridWrap");
-            var gridBox = this.gridBox();
             gridWrap.appendChild(gridBox);
             
             for (var i = 0; i < chord.notes.length; i++) {
-                this.drawNote(gridWrap, chord.notes[i]);
+                var note = this.svgGenerator.note(chord.notes[i]);
+                gridWrap.appendChild(note);
             }
             
             container.appendChild(item);
@@ -127,8 +56,9 @@ namespace swingChords {
         }
         
         private drawChordGroup(chordGroup: ChordGroup): void {
-            this.drawHeader(chordGroup.name);
-            var container = document.createElement("div");
+            var container: HTMLDivElement = document.createElement("div");
+            
+            this.drawHeader(chordGroup.name);            
             container.classList.add("contentWrap");
             this.body.appendChild(container);
             this.drawChords(chordGroup.chords, container);
